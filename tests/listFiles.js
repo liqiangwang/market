70ac1e50ad9b7e3a89a16bfa914140325c2262b0
yangfan <fayan@FelixPC.fareast.corp.microsoft.com>
2015-09-26 19:29:23 +0800

    add full files

---
+++ tests/listFiles.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var inspect = require('util').inspect;
+var os = require('os');
+var p = "../"
+fs.readdir(p, function (err, files) {
+    if (err) {
+        throw err;
+    }
+
+    files.map(function (file) {
+        return file;  //path.join(p, file)
+    }).filter(function (file) {
+        return fs.statSync(path.join(p, file)).isFile();
+    }).forEach(function (file) {
+        console.log("%s (%s)", file, path.extname(file));
+    });
+});
---
+++ tests/listFilesSync.js
@@ -0,0 +1,15 @@
+﻿var fs = require('fs');
+var path = require('path');
+var inspect = require('util').inspect;
+var os = require('os');
+
+var p = "../"
+var files = fs.readdirSync(p);
+files.map(function (file) {
+    return path.join(p, file);
+}).filter(function (file) {
+    return fs.statSync(file).isFile();
+}).forEach(function (file) {
+    console.log("%s (%s)", file, path.extname(file));
+});
+
